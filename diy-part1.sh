ec712be830e46a0776fa01b740c0829def4bfc5e
lekai63 <lekai63@gmail.com>
2020-11-12 00:13:01 +0800

    change to Lienol

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,10 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# for Lean
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# for Lienol
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
